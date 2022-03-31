d865ab895df93bc4ecaae98f304c934dde5170ea
Hamilton Morais <92332979+hamiltonmorais@users.noreply.github.com>
2022-03-31 11:18:09 -0300

    meu segundo commit

---
+++ 3.Corrida Tartarugas/main.js
@@ -0,0 +1,24 @@
+for (i = 0; i < 3; i++) {
+
+  let tempo = gets();
+  let velocidade = gets();
+
+  let arrayV = velocidade.split(" ");
+  
+  let maior = 0;
+  let nivel = 0;
+
+  maior = arrayV.reduce((a, b) => (
+    Math.max(a, b)
+  )) 
+
+  if (maior < 10) {
+    nivel = 1;
+  } else if (maior < 20) {
+    nivel = 2;
+  } else {
+    nivel = 3
+  }
+
+  console.log(nivel)
+}
